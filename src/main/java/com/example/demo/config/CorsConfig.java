b0bcbed43d05325ba372b46f643090867e121a6b
bbbii <hyeongjin1326@gmail.com>
2023-06-02 12:16:30 +0900

    update: [김형진] CorsConfig 경로 추가

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:8080", "http://localhost:8080")
+                .allowedOrigins("http://127.0.0.1:8080", "http://localhost:8080", "http://43.201.228.165")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
